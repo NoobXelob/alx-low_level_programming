b8d424fb67770ea25aa5cc3ebe8b0180317aab6c
NoobXelob <leonidesintell@gmail.com>
2023-12-14 16:47:29 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a text to the console using puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
